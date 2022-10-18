c6660b81e731c4404aa7274861e4ffa277264342
Felipe <felipedossantosflor@gmail.com>
2022-10-18 20:18:02 -0300

    @FelipeFlor [Task: Config appsettings, startup]

--- ToDoListAPI/Startup.cs
+++ ToDoListAPI/Startup.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -9,6 +10,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ToDoListAPI.Src.Contextos;
 
 namespace ToDoListAPI
 {
@@ -24,18 +26,26 @@ namespace ToDoListAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            // Configuração de Banco de dados
+            services.AddDbContext<ToDoListContexto>(opt =>
+            opt.UseSqlServer(Configuration["ConnectionStringsDev:DefaultConnection"]));
 
+            // Controladores
             services.AddControllers();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ToDoListContexto contexto)
         {
+            // Ambiente de Desenvolvimento
             if (env.IsDevelopment())
             {
+                contexto.Database.EnsureCreated();
                 app.UseDeveloperExceptionPage();
             }
 
+            // Ambiente de produção
+            contexto.Database.EnsureCreated();
             app.UseRouting();
 
             app.UseAuthorization();
