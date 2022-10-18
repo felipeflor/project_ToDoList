3b1c93b4da128faca00efe44e95a3e68af4d1f74
Felipe <felipedossantosflor@gmail.com>
2022-10-18 19:46:05 -0300

    @FelipeFlor [Task: Contexto]

--- ToDoListAPI/Src/Modelos/ListaModelo.cs
+++ ToDoListAPI/Src/Modelos/ListaModelo.cs
@@ -1,6 +1,29 @@
-﻿namespace ToDoListAPI.Src.Modelos
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ToDoListAPI.Src.Modelos
 {
-    public class ListaModelo
+
+    /// <summary>
+    /// <para>Resumo: Classe responsavel por representar tb_list no banco.</para>
+    /// <para>Criado por: Felipe Flor</para>
+    /// <para>Versão: 1.0</para>
+    /// <para>Data: 18/10/2022</para>
+    /// </summary>
+
+    [Table("tb_lists")]
+    public class Lista
     {
+        #region Atributos
+
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+
+        public int Id { get; set; }
+        public string Task { get; set; }
+        public bool Status { get; set; }
+
+        #endregion
+
     }
 }
